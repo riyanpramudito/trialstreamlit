9b9104cee206a2aea63a2ca2b8fc2bed2394ed57
riyanpramudito <riyanpramudito@gmail.com>
2025-05-16 09:11:52 +0100

    adding pandas dataframe

--- publish_web.py
+++ publish_web.py
@@ -1,7 +1,12 @@
 import streamlit as st
+import pandas as pd
 st.title("trial publish app dashboard")
 st.subheader("enjoy the first app")
 st.write("""this is my first steamlit apps
          hahahah!!
          really love it!!
          """)
+st.write("adding pandas dataframe")
+data = pd.DataFrame({'series 1': [1,2,3,4], 
+                     'series 2': [10,30,40,20]})
+st.write(data)
