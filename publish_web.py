e9915dcdf637bdc031165024bd67b769b8a03084
riyanpramudito <riyanpramudito@gmail.com>
2025-05-16 08:54:22 +0100

    Initial commit with .gitignore, app script, and requirements

---
+++ publish_web.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title("trial publish app dashboard")
+st.subheader("enjoy the first app")
+st.write("""this is my first steamlit apps
+         hahahah!!
+         really love it!!
+         """)
